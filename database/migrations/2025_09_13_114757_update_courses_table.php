9b5b9666f282de7d36fb8604fc3710fd706310ea
ramiznazar <ramiznazar600@gmail.com>
2025-10-01 06:56:16 -0700

    hel;

--- database/migrations/2025_09_13_114757_update_courses_table.php
+++ database/migrations/2025_09_13_114757_update_courses_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            $table->string('slug')->unique()->after('title');
+            // $table->string('slug')->unique()->after('title');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            //
+            $table->dropColumn('slug');
         });
     }
 };
